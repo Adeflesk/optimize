f3d8ce4702f80dc1d49d663180e6c8d34dfc3c4c
Adrian Corsini <glenflesk@gmail.com>
2014-03-21 00:57:34 +0000

    Added watch task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,29 @@ module.exports = function (grunt){
   //Load all grunt tasks matching the 'grunt=*' pattern
   require('load-grunt-tasks')(grunt, ['grunt-*']);
 
-  grunt.initConfig({});
-  grunt.registerTask('default', []);
-}
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+                 jshintrc: '.jshintrc'
+               },
+     all: ['Gruntfile.js','app/js/**/*.js','spec/**/*.js']
+
+    },
+
+
+    //Run: 'grunt watch' from commandline for this section to take effect
+    watch: {
+    scripts: {
+        files: '**/*.js',
+        tasks: ['jshint'],
+        options: {
+           interrupt: true
+           },
+           },
+           },
+
+  });
+  grunt.registerTask('default', ['jshint:all']);
+};
